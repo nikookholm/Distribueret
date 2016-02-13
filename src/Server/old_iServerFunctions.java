dbe3d0cdb2911abab49b892f7cbe6f8bca23d58b
amal q <zizja4ka@yahoo.dk>
2016-02-13 20:29:52 +0100

    Revert "com.cdyne.ws package"
    This reverts commit 87a1302cac9c76e9a450f639e50fc4186ab34734.

---
+++ src/Server/old_iServerFunctions.java
@@ -0,0 +1,16 @@
+//package Server;
+//
+//import java.rmi.Remote;
+//import java.rmi.RemoteException;
+//
+//import GalgeLogik.IGalgelogik;
+//import brugerautorisation.data.Bruger;
+//
+//public interface old_iServerFunctions extends Remote {
+//
+//	public Bruger login(String username, String password)
+//			throws RemoteException;
+//
+//	//public IGalgelogik accessGL() throws RemoteException;
+//
+//}

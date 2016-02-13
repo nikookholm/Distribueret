43c086d3187725acb42d316f9bdf46b6f32b69ca
Firestorm <Firestorm@DESKTOP-R0TJ84F>
2016-02-13 20:44:54 +0100

    Billeder + ændringer til GUI

---
+++ src/klient/guiTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package klient;
+
+/**
+ *
+ * @author Inferno
+ */
+public class guiTest extends javax.swing.JPanel {
+    public static void main(String[] arg){
+        guiGalge galge = new guiGalge();
+        galge.setVisible(true);
+        while(true);
+        
+        
+    }
+}

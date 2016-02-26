c7e6ac43c627441af6f8b668e8ec3bc6e694d459
amal q <zizja4ka@yahoo.dk>
2016-02-26 14:21:50 +0100

    tui

--- src/Klient/Klient.java
+++ src/Klient/Klient.java
@@ -1,5 +1,140 @@
 package Klient;
 
-public class Klient {
+import Server.iServerFunctions;
+import brugerautorisation.data.Bruger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
 
+public class Klient {
+    
+   private iServerFunctions isf;
+   private TUI tui;
+   private Bruger bruger;
+    
+    public Klient(){
+         initialize();
+         login();
+         gameloop();
+         showResult();
+         restart();
+         
+        
+    }
+    
+    public void main(String[] args){
+        
+        new Klient();
+        
+    }
+    
+    private void initialize(){
+        
+        try {
+            isf = (iServerFunctions) Naming.lookup("rmi://localhost/galgeleg");
+        } catch (Exception ex) { }
+        
+        tui = new TUI();
+        
+    }
+
+    private void login() {
+        
+         String[] result = tui.login();
+         
+       try {
+           bruger =  isf.login(result[0], result[1]);
+           tui.print("du er login som: " + bruger.brugernavn);
+       } catch (RemoteException ex) {
+           
+           tui.print(ex.getMessage());
+          
+       }
+
+    }
+    
+    
+       private void gameloop() {
+           
+           if(bruger != null){
+               
+              
+           
+               try {
+                    isf.nulstil();
+                    
+                   while(!isf.erSpilletSlut()){
+                      
+                       String gætBokstav = tui.gætBokstav();
+                       
+                       isf.gætBogstav(gætBokstav);
+
+                   }
+      
+               } catch (RemoteException ex) {
+                   Logger.getLogger(Klient.class.getName()).log(Level.SEVERE, null, ex);
+               }
+           } else{
+               tui.print("man skal være login!");
+           }
+           
+        
+          
+        }
+
+    private void showResult() {
+        
+       try {
+           if (isf.erSpilletVundet()){
+               
+               tui.print("Dit result: DU HAR VUNDET MED " + isf.getBrugteBogstaver().size() + " gæt!");
+           }
+           
+           if (isf.erSpilletTabt()){
+               
+               tui.print("Dit result: DU HAR TABT MED " + isf.getBrugteBogstaver().size() + " gæt!");
+               
+           }
+           // du har vuindet/tabt + resultat
+           
+        
+       } catch (RemoteException ex) {
+           Logger.getLogger(Klient.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       
+     
+       
+    
+        
+    }
+    
+    public void restart() {
+        
+       try {
+           if(isf.erSpilletSlut()){
+               
+               if (tui.confirmRestart()){
+                   
+                   gameloop();
+                   
+               }
+               
+           }
+       } catch (RemoteException ex) {
+           Logger.getLogger(Klient.class.getName()).log(Level.SEVERE, null, ex);
+       }
+        
+        
+    }
+
+ 
+    
+    
+   
 }
---
+++ src/Klient/TUI.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Klient;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author amal
+ */
+public class TUI {
+    
+   private Scanner   scn = new Scanner(System.in);;
+    
+    public String[] login(){
+        
+        // result[0]  retunerer navn
+        // result[1] returnerer password;
+        
+      
+        System.out.println("indtest din navn: ");
+        String name = scn.nextLine();
+        System.out.println("indtest dit password: ");
+        String password = scn.nextLine();
+        
+        String[] result = new String[2];
+        result[0] = name;
+        result[1] = password;
+        
+                return result;
+                
+    }
+    
+    public void print(String text){
+       
+        System.out.println( text );
+    }
+    
+    public String gætBokstav(){
+        
+        print("indtest en bokstave du vil gæt: " );
+ 
+        return  scn.nextLine();
+    }
+    public boolean confirmRestart(){
+        
+        print("tryk \"Y\" for restart");
+        
+        if (scn.nextLine().toLowerCase().equals("y") ){
+            
+            return true;
+        }
+            
+        return false;
+        
+    }
+    
+}
